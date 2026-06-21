0a89cd459a77a236ab242c76bd71de01baeff660
Scott Bonds <scott@ggr.com>
2026-06-20 17:08:45 -0700

    Restore hardcoded lib path for local dev

--- Package.swift
+++ Package.swift
@@ -11,7 +11,10 @@ let package = Package(
         ),
         .executableTarget(
             name: "polyptych",
-            dependencies: ["Clibmpv"]
+            dependencies: ["Clibmpv"],
+            linkerSettings: [
+                .unsafeFlags(["-L", "/nix/store/03zfgn8fi73mrnr848y69vhb6nn04zwp-mpv-0.41.0/lib"])
+            ]
         )
     ]
 )
