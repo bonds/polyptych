7ef185e92dbcd726bb197fd27abd049d27ac8cde
Scott Bonds <scott@ggr.com>
2026-06-21 21:15:47 -0700

    dynaudnorm: maxgain=100

--- Sources/polyptych/MPVController.swift
+++ Sources/polyptych/MPVController.swift
@@ -38,7 +38,7 @@ final class MPVController: @unchecked Sendable {
         mpv_set_option_string(mpv, "vo", "libmpv")
         mpv_set_option_string(mpv, "hwdec", "no")
         mpv_set_option_string(mpv, "audio-buffer", "0.05")
-        mpv_set_option_string(mpv, "af", "dynaudnorm=targetrms=0.3:maxgain=30")
+        mpv_set_option_string(mpv, "af", "dynaudnorm=targetrms=0.3:maxgain=100")
         mpv_set_option_string(mpv, "video-sync", "audio")
         mpv_set_option_string(mpv, "osd-level", "1")
         mpv_set_option_string(mpv, "osd-align-x", "center")
