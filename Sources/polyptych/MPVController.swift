3dc3ae9d3b69d578c12a5cc32ced7e052f27e551
Scott Bonds <scott@ggr.com>
2026-06-24 20:29:05 -0700

    Replace dynaudnorm with loudnorm (EBU R128, matches YouTube's loudness normalization exactly) + volume=1.3 offset

--- Sources/polyptych/MPVController.swift
+++ Sources/polyptych/MPVController.swift
@@ -38,7 +38,7 @@ final class MPVController: @unchecked Sendable {
         mpv_set_option_string(mpv, "vo", "libmpv")
         mpv_set_option_string(mpv, "hwdec", "no")
         mpv_set_option_string(mpv, "audio-buffer", "0.05")
-        mpv_set_option_string(mpv, "af", "dynaudnorm=targetrms=0.3:maxgain=100,volume=2.0")
+        mpv_set_option_string(mpv, "af", "loudnorm=I=-14:LRA=11:TP=-1.5,volume=1.3")
         mpv_set_option_string(mpv, "video-sync", "audio")
         mpv_set_option_string(mpv, "osd-level", "1")
         mpv_set_option_string(mpv, "osd-align-x", "center")
