63c0e254c0f2711c0de865e7a997784d06fb2ae9
Scott Bonds <scott@ggr.com>
2026-06-27 19:07:53 -0700

    Release v0.7.0

--- flake.nix
+++ flake.nix
@@ -18,7 +18,7 @@
         {
           default = pkgs.stdenv.mkDerivation {
             pname = "polyptych";
-            version = "0.6.0";
+            version = "0.7.0";
             src = self;
 
           buildInputs = with pkgs; [
